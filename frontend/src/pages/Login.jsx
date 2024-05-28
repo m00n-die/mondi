fd293f72a221945cf05c62b96d0e354184b450d1
m00n-die <reginaldrock219@gmail.com>
2024-05-28 23:23:29 +0100

    Initialized the frontend directory

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+    return <div>
+        <h1>Login</h1>
+        <p>The Login Page</p>
+    </div>
+}
+
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+function Register() {
+    return <div>
+        <h1>Register</h1>
+        <p>The Registration Page</p>
+    </div>
+}
+
+export default Register;
